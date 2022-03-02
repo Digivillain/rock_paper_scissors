bfe1d509f6e92ec94ed0de9e1ee8f43fbd85e67f
Tyler <tyler.ward1199@gmail.com>
2022-03-02 18:13:43 -0500

    Added temporary ui and make first to five work

--- scripts.js
+++ scripts.js
@@ -1,6 +1,13 @@
 let playerScore = 0;
 let computerScore = 0;
 let result;
+let roundResult = document.createElement('p');
+const main = document.querySelector('main');
+const pScore = document.querySelector('.player-score');
+const cScore = document.querySelector('.computer-score');
+const rockBtn = document.querySelector('.rock');
+const paperBtn = document.querySelector('.paper');
+const scissorsBtn = document.querySelector('.scissors');
 
 function computersMove() {                 // returns the computer's move by declaring an array and randomizing through the length
     let arr = ['Rock', 'Paper', 'Scissors'];
@@ -9,41 +16,84 @@ function computersMove() {                 // returns the computer's move by dec
     return random.toLowerCase();
 }
 
-function playersMove() {
-    let move = prompt('What is your move? Rock, paper, or scissors?');
-    if (move === null || move === '') {
-        playersMove()
-    } else {
-        return move.toLowerCase();
-    }
-}
-
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === 'rock' && computerSelection === 'scissors' ||
         playerSelection === 'scissors' && computerSelection === 'paper' ||
         playerSelection === 'paper' && computerSelection === 'rock') {
             playerScore++;
-            return result = `YOU WIN, ${playerSelection} beats ${computerSelection}.`;
+            return result = `Round won, ${playerSelection} beats ${computerSelection}.`;
     } else if (playerSelection === 'rock' && computerSelection === 'paper' ||
         playerSelection === 'scissors' && computerSelection === 'rock' ||
         playerSelection === 'paper' && computerSelection === 'scissors') {
             computerScore++;
-            return result = `YOU LOSE, ${computerSelection} beats ${playerSelection}.`;
+            return result = `Round lost, ${computerSelection} beats ${playerSelection}.`;
     } else {
-        return result = `Tie! You both chose ${playerSelection}.`;
+        return result = `Tie round! You both chose ${playerSelection}.`;
     }
 }
 
-function game() {
-    for (i = 0; i < 5; i++) {
-        playRound(playersMove(), computersMove());
-        console.log(result);
+function updateScore() {
+    cScore.textContent = `Computer score: ${computerScore}`;
+    pScore.textContent = `Your score: ${playerScore}`;
+
+    if (playerScore === 5) {
+        let gameOver = document.createElement('h1');
+        main.appendChild(gameOver);
+        gameOver.textContent = 'You win! Please refresh to play again.';
+        disableButton();
+    } else if (computerScore === 5) {
+        let gameOver = document.createElement('h1');
+        main.appendChild(gameOver);
+        gameOver.textContent = 'You lose! Please refresh to play again.';
+        disableButton();
     }
-    console.log(`You scored ${playerScore} and the computer scored ${computerScore}.`);
+}
+updateScore();
+
+function showRoundResult() {
+    main.appendChild(roundResult);
+    roundResult.textContent = result;
+
+    if (playerScore >= 5 || computerScore >= 5) {
+        roundResult.remove();
+    }
 }
 
-game();
-// return `Tie, you both picked ${playerSelection}.`;
+rockBtn.addEventListener('click', () => {
+    playRound('rock', computersMove());
+    updateScore();
+    showRoundResult()
+});
+
+paperBtn.addEventListener('click', () => {
+    playRound('paper', computersMove());
+    updateScore();
+    showRoundResult()
+});
+
+scissorsBtn.addEventListener('click', () => {
+    playRound('scissors', computersMove());
+    updateScore();
+    showRoundResult()
+});
+
+
+function disableButton() {
+    rockBtn.disabled = true;
+    paperBtn.disabled = true;
+    scissorsBtn.disabled = true;
+}
+
+// function game() {
+//     for (i = 0; i < 5; i++) {
+//         playRound(playersMove(), computersMove());
+//         console.log(result);
+//     }
+//     console.log(`You scored ${playerScore} and the computer scored ${computerScore}.`);
+// }
+
+// game();
+
 
 
 
